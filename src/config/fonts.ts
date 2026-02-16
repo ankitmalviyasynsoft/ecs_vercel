22439c3309a32cbeeb9e738f19cbc40d175d481d
Ankit Malviya <ankitmalviya42@gmail.com>
2026-02-16 10:40:56 +0530

    fonts changes

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -2,13 +2,23 @@ import localFont from 'next/font/local'
 
 export const kaffFont = localFont({
   src: [
+    // {
+    //   path: '../../public/assets/fonts/Kaff/29LTKaff-Regular.otf',
+    //   weight: '400',
+    //   style: 'normal',
+    // },
+    // {
+    //   path: '../../public/assets/fonts/Kaff/29LTKaff-SemiBold.otf',
+    //   weight: '600',
+    //   style: 'normal',
+    // },
     {
-      path: '../../public/assets/fonts/Kaff/29LTKaff-Regular.otf',
+      path: './fonts/Kaff/29LTKaff-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/Kaff/29LTKaff-SemiBold.otf',
+      path: './fonts/Kaff/29LTKaff-SemiBold.otf',
       weight: '600',
       style: 'normal',
     },
